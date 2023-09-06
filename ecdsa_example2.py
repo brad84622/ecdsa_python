aa847bf31fb60452c64769be7803651f380b51c9
brad84622 <brad84622@gmail.com>
2023-09-07 04:29:06 +0800

    fix KAT version

--- ecdsa_example2.py
+++ ecdsa_example2.py
@@ -1,5 +1,8 @@
 import hashlib
 from ecdsa import SigningKey, NIST256p , numbertheory
+from ecdsa.ellipticcurve import Point
+import ecdsa
+
 import random
 import os
 
@@ -24,23 +27,31 @@ file_name = "output"
 
 idx = 0
 #=====rand=====
-while (idx < 5):
-    private_key = SigningKey.generate(curve=NIST256p)
+# while (idx < 100):
+    #dbg===
+while (idx < 1):
+    idx+=1
+    #dbg===
+    # private_key = SigningKey.generate(curve=NIST256p)
+    
+
+    # k_int = random.randint(order-32, order-1)
+
+    #  ====KAT======
+    private_key_hex = "C9AFA9D845BA75166B5C215767B1D6934E50C3DB36E89B127B8A622B120F6721"
+    private_key_bytes = bytes.fromhex(private_key_hex)
+    private_key = SigningKey.from_string(private_key_bytes, curve=NIST256p)
+    k_hex = "A6E3C57DD01ABE90086538398355DD4C3B17AA873382B0F24D6129493D8AAD60"
+    k_int = int(k_hex,16)
+
+
     curve_name = private_key.curve.name
-    print(f"curve_name = {curve_name}")
+    G = private_key.curve.generator
+    print(f"G = {G.x()} , {G.y()}")
+    print(f"curve_name = {curve_name}")   
     order = private_key.curve.generator.order()
     print(f"order_hex = {hex(order)}")
     print(f"order_int = {int(order)}")
-    k_int = random.randint(order-32, order-1)
-
-    #  ====KAT======
-    # private_key_hex = "C9AFA9D845BA75166B5C215767B1D6934E50C3DB36E89B127B8A622B120F6721"
-    # private_key_bytes = bytes.fromhex(private_key_hex)
-    # private_key = SigningKey.from_string(private_key_bytes, curve=NIST256p)
-    # k_hex = "A6E3C57DD01ABE90086538398355DD4C3B17AA873382B0F24D6129493D8AAD60"
-    # k_int = int(k_hex,16)
-
-
     public_key = private_key.verifying_key
 
     private_key_hex = private_key.to_string().hex()
@@ -52,31 +63,37 @@ while (idx < 5):
     uy = int.from_bytes(public_key_bytes[32:], byteorder='big')
     ux_hex = hex(ux)
     uy_hex = hex(uy)
-
+    Q = public_key.pubkey.point
 
     print(f"Private Key: {private_key_hex}")
     print(f"Public Key: {public_key_hex}")
     print(f"Public Key (ux): {ux_hex}")
     print(f"Public Key (uy): {uy_hex}")
 
-    message_length = random.randint(0,1024)
-    message = os.urandom(message_length)
+    # message_length = random.randint(0,1024)
+    # message = os.urandom(message_length)
+    message = b"sample"
+    message_length =len(message)
     print(f"msg_len = {message_length}")
     print(f"msg = {(message.hex())}")
-    # message = b"sample"
     e_byte = hashlib.sha256(message).digest()
     e_hex = e_byte.hex()
-
+    print(f"e_hex = {(e_hex)}")
+    print(f"e_int = {(int(e_hex,16))}")
 
     signature = private_key.sign(message,hashfunc=hashlib.sha256,k=k_int)
+    # r = signature.r
+    # s = signature.s
     r, s = signature[:32], signature[32:]
-
+    print(f"signature = {signature}")
     # r_hex = "EFD48B2AACB6A8FD1140DD9CD45E81D69D2C877B56AAF991C34D0EA84EAF3716"
     # s_hex = "F7CB1C942D657C41D436C7A1B6E29F65F3E900DBB9AFF4064DC4AB2F843ACDA8"
     # r = int(r_hex,16)
 
     print(f"r = {r.hex()}")
     print(f"s = {s.hex()}")
+    print(f"r_int = {int(r.hex(),16)}")
+    print(f"s_int = {int(s.hex(),16)}")
     print("signature:")
     print(signature.hex())
 
@@ -84,10 +101,28 @@ while (idx < 5):
     s_int = int.from_bytes(s, byteorder='big')
     print(f"s_int = {s_int}")
     print(f"order = {order}")
-    c_sinv = multiplicative_inverse(s_int, order)
+    c_sinv = multiplicative_inverse(s_int, order) #correct
     print(f"c = s^-1 mod n = {c_sinv}")
-    cal_u1 = ((int(e_hex,16) * c_sinv) % order)
+    cal_u1 = ((int(e_hex,16) * c_sinv) % order)  
+    # e*c = 5536788856131197834474991438633800834774713107866660568132691507817751225680111716911624993594860029947175619565670227373506734536293453280022460487482453
+    # mod order : 76802929953564841014745990121047686326403956053371139132425205714078128822337
     print(f"u1 = e*c mod n = {cal_u1}")
+    r_int = int.from_bytes(r, byteorder='big')
+    cal_u2 = ((r_int * c_sinv) % order)
+    # r*c = 7580518420274355287472345797030454017337070184722691795483088616946757365108053876377051026766960438892936939099563588761110220825869819761502817137345842
+    # mod order = 32955858153445698164125015627026694558773479180781844519416553310382593614109
+    # cal_u2 = ((int(r) * int(c_sinv)) % order)
+    print(f"u2 = r*c mod n = {cal_u2}")
+    gx = 48439561293906451759052585252797914202762949526041747995844080717082404635286
+    R = (cal_u1*G+cal_u2*Q) 
+    v = R.x() % order
+    print(f"cal_u1*gx = {cal_u1*G}")
+    print(f"cal_u2*ux = {cal_u2*Q}")
+    print(f"R = {R}")
+    print(f"v = {v}")
+    print(f"r = {int(r.hex(),16)}")
+    if(v != int(r.hex(),16)):
+        raise ValueError("R != r")
     if( (order - cal_u1 <=32) and (order - cal_u1 >0) ):
     # if( cal_u1 >= 0 ):
         print("get the case!")
@@ -114,7 +149,6 @@ while (idx < 5):
     try:
         public_key.verify(signature, message,hashfunc=hashlib.sha256)
         print("signature valid")
-
         print(f"R = {r.hex()}")
         print(f"S = {s.hex()}")
         print(f"Qx = {ux_hex}")
@@ -125,6 +159,7 @@ while (idx < 5):
 
     except :
         print("signature invalid , ERROR!!!")
+        print(ValueError)
 
 
 
